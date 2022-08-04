1f9a02c94fcea30360211a9dc29de9b7e4e399b3
JJiWoNing <jjiwoning12@gmail.com>
2022-08-04 18:14:21 +0900

    8/4 Study Spring Transaction Propagation

---
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,37 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired LogRepository logRepository;
+
+    /**
+     * memberService @Transactional off
+     * memberRepository @Transactional on
+     * logRepository @Transactional on
+     */
+    @Test
+    void outerTxOff_success(){
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then : 모든 데이터 정상 저장
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+}
